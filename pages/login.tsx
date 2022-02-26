0743607fe65779820809d2671ee1a0808fef1030
Anthony Ardi Sukotjo <aas10023@nyu.edu>
2022-02-26 11:05:27 -0500

    basic skeleton

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,32 @@
 import React from "react"
+import {Col, FloatingLabel, Row, Form} from "react-bootstrap";
 const Login = ()=>{
-    return (<h1>login page</h1>);
+    return (<div>
+        <h1>login page</h1>
+        <Col>
+            <Form>
+                <h3>Sign In</h3>
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <p className="forgot-password text-right">
+                    Forgot <a href="#">password?</a>
+                </p>
+            </Form>
+
+        </Col>
+    </div>);
 }
 export default Login;
