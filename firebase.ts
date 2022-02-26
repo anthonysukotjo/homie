5bbeaa0ccfc092173b74a856a9d3b2c0a0abcbda
Anthony Ardi Sukotjo <aas10023@nyu.edu>
2022-02-26 15:26:24 -0500

    attempt at auth

--- firebase.ts
+++ firebase.ts
@@ -1,15 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import {getAuth} from "@firebase/auth";
+import {getFirestore} from "@firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    apiKey: "AIzaSyBR3Hcc32iiZ5Jk-fpLl0t7vz54lt4Ni4s",
+    authDomain: "homie-7df1b.firebaseapp.com",
+    projectId: "homie-7df1b",
     storageBucket: "homie-7df1b.appspot.com",
     messagingSenderId: "829211595380",
     appId: "1:829211595380:web:ca89f9723fa2cf97673721",
@@ -18,4 +20,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export {  app};
+console.log(app.name ? 'Firebase Mode Activated!' : 'Firebase not working :(');
