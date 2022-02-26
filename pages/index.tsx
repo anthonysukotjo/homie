be9a3dd79943e8c2b9e05c17454bda4a0638b2fb
CCClia <yuewenkapa@gmail.com>
2022-02-26 17:49:24 -0500

    in progress

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,67 +2,59 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import {Button, Col} from "react-bootstrap";
+import { Button, Col } from "react-bootstrap";
+import { fontGrid } from '@mui/material/styles/cssUtils';
+import styled from 'styled-components';
 
 const Home: NextPage = () => {
     return (
 
         <div>
             <div >
-                <h1 style={{ marginTop: "200px", fontSize: "45px" }}>WELCOME HOMIE
+                <h1 style={{
+                    marginTop: "200px", fontSize: "60px",
+                }}>WELCOME TO HOMIE
                 </h1>
-                <Col>
 
+
+
+                <Col>
                     <div >
                         <a
                             href={"/login"} >
                             <div
-
-
                                 className="loginbutton"
                             >
-
                                 Login
-
-
-
-
                             </div>
                         </a>
                     </div>
+
+                    {/* <button className="loginbutton"
+                        onclick="/login">
+                        login
+                    </button> */}
+
 
                     <div className='button'>
                         <a
                             href={"/signup"}>
                             <div
-
-
                                 className="signupbutton"
                             >
-
                                 Sign Up
-
-
-
-
                             </div>
                         </a>
                     </div>
 
+                    {/* <a id="houseList" href='/login'> <img id="house" src='house.png' alt='house' width={120} height={130} style={{ marginTop: "100px" }}
+                    /> </a> */}
 
-
-
-
-                    <div id='house_icon'>
-                        <img id="house" src='house.png' alt='house' width={120} height={130} style={{ marginTop: "100px" }}
-                        />
-
-
-
-                    </div>
-
+                    <img id="house" src='house.png' alt='house' width={120} height={130} style={{ marginTop: "100px" }}
+                    />
 
                 </Col>
+
             </div>
         </div>
 
@@ -70,5 +62,6 @@ const Home: NextPage = () => {
 
     )
 }
+
 
 export default Home;
--- pages/loading.tsx
+++ pages/loading.tsx
@@ -1,24 +1,24 @@
 import React from "react"
-import {Col, FloatingLabel, Row, Form} from "react-bootstrap";
+import { Col, FloatingLabel, Row, Form } from "react-bootstrap";
 import TimePicker from 'react-bootstrap-time-picker';
-import {Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 function Load() {
 
 
-    return (
-      <div >
-    
-      <h1 style={{marginTop:"200px", fontSize:"45px"}}> Helping you locate a homie...</h1>
-      
+  return (
+    <div >
+
+      <h1 style={{ marginTop: "200px", fontSize: "45px" }}> Helping you locate a homie...</h1>
+
       {/* need to add loading homie image here */}
-   
-      </div>  
-    )
-    
+
+    </div>
+  )
+
     ;
-  }
-  
+}
+
 
 
 export default Load;
