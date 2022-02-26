3302ecb0f94c6d57d4968f84e71c1235bebc2273
Anthony Ardi Sukotjo <aas10023@nyu.edu>
2022-02-26 17:58:42 -0500

    collect data from dv

--- pages/homieList.tsx
+++ pages/homieList.tsx
@@ -1,14 +1,39 @@
-import React from "react"
+import React, {useState,useEffect} from "react"
 import {Col, FloatingLabel, Row, Form} from "react-bootstrap";
 import TimePicker from 'react-bootstrap-time-picker';
 import {Button} from "react-bootstrap";
+import { app } from './../firebase';
+import {collection, query, where, getDocs, getFirestore} from "firebase/firestore";
 
-function Homies() {
+
+const Homies = () => {
+
+    const [homieData, setHomieData] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                let tempData: any = [];
+                const q = query(collection(getFirestore(app), "location"));
+                const querySnapshot = await getDocs(q);
+                querySnapshot.forEach((doc) => {
+                    // doc.data() is never undefined for query doc snapshots
+                    console.log(doc.id, " => ", doc.data());
+                    let temp = doc.data();
+                    tempData.push(temp);
+                });
+                setHomieData(tempData);
+            } catch (error) {
+                console.error(error);
+            }
+        })();
+    }, []);
 
 
     return (
       <Col style={{textAlign:"center"}}> 
       <h1 style={{marginTop:"50px", fontSize:"40px"}}> Your Homies</h1>
+          <h1>{JSON.stringify(homieData)}</h1>
 
       {/* List of Homies */}
 
