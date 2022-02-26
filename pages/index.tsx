b212ef7eb6bddf2ae1b4e96846e2090472e2b2d9
CCClia <100012430+kapa-moon@users.noreply.github.com>
2022-02-26 13:40:48 -0500

    Revert "change"

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,63 +2,40 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { Button, Col } from "react-bootstrap";
+import {Button, Col} from "react-bootstrap";
 
 const Home: NextPage = () => {
-    return (
+  return (
 
-        <div>
-            <div >
-                <h1 style={{ marginTop: "200px", fontSize: "45px" }}>WELCOME HOMIE
-                </h1>
-                <Col>
+      <div>
+          <div >
+              <h1 style={{marginTop:"200px", fontSize:"45px" }}>WELCOME HOMIE
+              </h1>
+              <Col>
 
-                    <div >
-                        <a
-                            href={"/login"}>
-                            <div
+                  <div >
+                      <Button
+                          style={{marginTop:"50px", width:"100px"}}
+                          href={"/login"}>
 
-
-                                className="loginbutton"
-                            >
-
-                                Login
-
-
-
-
-                            </div>
-                        </a>
-                    </div>
-
-
-
-
-                    <div  >
-                        <Button
-                            style={{ marginTop: "50px", width: "180px" }}
-                            href={"/signup"}
-                        >
-                            Sign up
-                        </Button>
-                    </div>
-
-                    <div id='house_icon'>
-                        <img id="house" src='house.png' alt='house' width={120} height={130} style={{ marginTop: "100px" }}
-                        />
+                          Login
+                      </Button>
+                  </div>
+                  <div  >
+                  <Button
+                      style={{marginTop:"50px", width:"100px"}}
+                      href={"/signup"}
+                  >
+                      Sign up
+                  </Button>
+              </div>
+              </Col>
+          </div>
+      </div>
 
 
 
-                    </div>
-
-
-                </Col>
-            </div>
-        </div>
-
-
-
-    )
+  )
 }
 
 export default Home;
