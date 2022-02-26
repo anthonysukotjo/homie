cc6b13e04c2cb209937a2369c8b0f76c7d29e742
Anthony Ardi Sukotjo <aas10023@nyu.edu>
2022-02-25 23:00:22 -0500

    initial commit

---
+++ pages/login.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+const Login = ()=>{
+    return (<h1>login page</h1>);
+}
+export default Login;
---
+++ pages/signup.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+const SignUp = ()=>{
+    return (<h1>Signup</h1>);
+}
+export default SignUp;
