694aa0f39e084a31664529ce58edde8c394be42b
megalo999 <myc9078@nyu.edu>
2022-02-26 20:35:11 -0500

    signin

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {Col, FloatingLabel, Row, Form} from "react-bootstrap";
+import styled from 'styled-components';
 
 const SignUp = ()=>{
     return (<div>
@@ -33,3 +34,16 @@ const SignUp = ()=>{
     </div>);
 }
 export default SignUp;
+
+const Button = styled.button`
+  background-color: #b8b8ff;
+  color: black;
+  border: none
+  font-family: 'Signika Negative', sans-serif;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer;
+  filter: drop-shadow(5px 5px 4px #4444dd);
+`;
